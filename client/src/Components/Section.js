1b69e9937b7a7e6e28bd68150a9c4f3be0476a49
Dyne Park_PC <parkdyne.work@gmail.com>
2021-12-06 06:50:03 +0900

    responsive

--- client/src/Components/Section.js
+++ client/src/Components/Section.js
@@ -3,18 +3,16 @@ import Card from "./Card";
 import styles from "./Section.module.css";
 
 function Section({ publisher, webtoons }) {
-    return (
-        <div className={styles.section}>
-            <div className={`${styles.title} ${styles[publisher]}`}>
-                {publisher}
-            </div>
-            <div className={styles.cards}>
-                {webtoons.map((webtoon) => (
-                    <Card webtoon={webtoon} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.section}>
+      <div className={`${styles.title} ${styles[publisher]}`}>{publisher}</div>
+      <div className={styles.cards}>
+        {webtoons.map((webtoon) => (
+          <Card key={webtoon.id} webtoon={webtoon} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Section;
--- client/src/Components/WIP.js
+++ client/src/Components/WIP.js
@@ -4,31 +4,32 @@ import Gnb from "./Gnb";
 import { Link } from "react-router-dom";
 
 function WIP() {
-    const tempStyle = {
-        background: "#fff",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        overflow: "hidden",
-    };
+  const tempStyle = {
+    background: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  };
 
-    const tempStyle2 = {
-        maxWidth: "700px",
-    };
-    return (
-        <div>
-            <Header />
-            <Gnb />
-            <Link to="/">
-                <div style={tempStyle}>
-                    <img
-                        style={tempStyle2}
-                        src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg.webp"
-                    />
-                </div>
-            </Link>
+  const tempStyle2 = {
+    maxWidth: "700px",
+  };
+  return (
+    <div>
+      <Header />
+      <Gnb />
+      <Link to="/">
+        <div style={tempStyle}>
+          <img
+            style={tempStyle2}
+            src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg.webp"
+            alt="work in progress"
+          />
         </div>
-    );
+      </Link>
+    </div>
+  );
 }
 
 export default WIP;
