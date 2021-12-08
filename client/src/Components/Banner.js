a2710fd0ef3983cfc8608d60b595324dd1181a62
DynePark <parkdyne.work@gmail.com>
2021-12-08 20:34:17 +0900

    banner error fix & backend login

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -2,37 +2,36 @@ import React from "react";
 import styles from "./Banner.module.css";
 
 function Banner() {
-  const [slide, setSlide] = useState(0);
-  return (
-    <div className={styles.banner}>
-      <div className={styles.container}>
-        <button className={styles.left}>
-          <i class="fas fa-chevron-left" />
-        </button>
-        <button className={styles.right}>
-          <i class="fas fa-chevron-right" />
-        </button>
-        <div className={styles.item}>
-          <img
-            src="https://thumbs.dreamstime.com/z/merry-christmas-colorful-balls-banner-decoration-stars-backgroun-background-text-77218307.jpg"
-            alt="a"
-          />
+    return (
+        <div className={styles.banner}>
+            <div className={styles.container}>
+                <button className={styles.left}>
+                    <i class="fas fa-chevron-left" />
+                </button>
+                <button className={styles.right}>
+                    <i class="fas fa-chevron-right" />
+                </button>
+                <div className={styles.item}>
+                    <img
+                        src="https://thumbs.dreamstime.com/z/merry-christmas-colorful-balls-banner-decoration-stars-backgroun-background-text-77218307.jpg"
+                        alt="a"
+                    />
+                </div>
+                <div className={styles.item}>
+                    <img
+                        src="https://thumbs.dreamstime.com/z/christmas-banner-gingerbread-man-cookies-mulled-wine-holiday-decorations-fir-branches-festive-bokeh-lighting-dark-80838264.jpg"
+                        alt="a"
+                    />
+                </div>
+                <div className={styles.item}>
+                    <img
+                        src="https://thumbs.dreamstime.com/z/christmas-decoration-banner-snowy-pine-cones-fir-branch-lights-77616585.jpg"
+                        alt="a"
+                    />
+                </div>
+            </div>
         </div>
-        <div className={styles.item}>
-          <img
-            src="https://thumbs.dreamstime.com/z/christmas-banner-gingerbread-man-cookies-mulled-wine-holiday-decorations-fir-branches-festive-bokeh-lighting-dark-80838264.jpg"
-            alt="a"
-          />
-        </div>
-        <div className={styles.item}>
-          <img
-            src="https://thumbs.dreamstime.com/z/christmas-decoration-banner-snowy-pine-cones-fir-branch-lights-77616585.jpg"
-            alt="a"
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Banner;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,74 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const argon2 = require("argon2");
 
+require("dotenv").config();
+const Mongodb_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("build"));
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/../index.html");
+let db;
+MongoClient.connect(Mongodb_URL, (error, client) => {
+    if (error) return console.log("MongoDB ERROR!!!");
+    db = client.db("WebtoonMoa");
 });
 
 app.listen(PORT, () => {
     console.log("server on!");
 });
+
+app.use(express.static("build"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/../index.html");
+});
+
+// 회원가입
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+    const hash = await argon2.hash(password);
+    db.collection("User")
+        .find({ email: email })
+        .toArray((req, result) => {
+            if (result.length !== 0) {
+                res.status(403).send(
+                    "<script>alert('이메일이 이미 존재합니다.');location.href='/';</script>"
+                );
+            } else {
+                db.collection("User").insertOne(
+                    { username, email, password: hash },
+                    (error, client) => {
+                        console.log("저장완료");
+                    }
+                );
+                res.send(
+                    "<script>alert('회원가입 완료!');location.href='/';</script>"
+                );
+            }
+        });
+});
+
+// 로그인
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    db.collection("User")
+        .find({ email: email })
+        .toArray(async (req, result) => {
+            if (result.length === 0) {
+                res.status(403).send(
+                    "<script>alert('아이디를 찾지 못했습니다.');location.href='/';</script>"
+                );
+            }
+            if (await argon2.verify(result[0].password, password)) {
+                res.send(
+                    "<script>alert('로그인 성공!');location.href='/';</script>"
+                );
+            } else {
+                res.status(403).send("패스워드가 틀립니다.");
+            }
+        });
+});
+
+app.get("/list", (req, res) => {});
